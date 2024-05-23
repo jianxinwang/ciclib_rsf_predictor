e98a9ba34b9bb9c7e67495d6bdce43f94ec815aa
ubjw24 <jianxinwang@users.noreply.github.com>
2024-05-23 16:52:24 -0400

    Update script

--- mk.KM.plot.R
+++ mk.KM.plot.R
@@ -1,13 +1,15 @@
 
 # make a Kaplan-Meier plot
-make.KM.plot <- function(survDf,  grouping.var = 'Group', surv.type = 'PFS', event = 'Status',  time.unit = 'Months', label.x = NULL, label.y = NULL, ...) {
+make.KM.plot <- function(survDf,  grouping.var = 'Group', surv.type = 'PFS', event = 'Status', 
+                         time.unit = 'Months', plot.title = NULL, legendLabs = NULL,
+                         label.x = NULL, label.y = NULL, palette = NULL, ...) {
   library("survival")
   library("survminer")
   
   fmla <- as.formula(paste('Surv(', surv.type, ', ', event, ') ~ ', grouping.var))
   survfit <- survfit(fmla, data = survDf)
   
-  medianLabel <- paste0("\t", gsub('.*?=', '', rownames(median(survfit))), ': ', format(round(median(survfit), 2), nsmall = 2), collapse = "\n")
+  medianLabel <- paste0("\t", gsub('.*?=', '', rownames(median(survfit))), ' = ', format(round(median(survfit), 2), nsmall = 2), collapse = "\n")
   medianLabel <- gsub('NA', 'Not reached', medianLabel)
   
   # get HR to put on K-M plot
@@ -26,7 +28,7 @@ make.KM.plot <- function(survDf,  grouping.var = 'Group', surv.type = 'PFS', eve
   coefMatrix <- matrix(x$coefficients, ncol = 5)
   
   # Identify the most significant contrast
-  ind <- which(coefMatrix[, 5] == min(coefMatrix[, 5]))
+  ind <- which(coefMatrix[, 5] == min(coefMatrix[, 5], na.rm = TRUE))
   
   hr <- signif(x$conf.int[ind, 'exp(coef)'], 2) 
   hr.low95 <- signif(x$conf.int[ind,"lower .95"], 2)
@@ -35,30 +37,33 @@ make.KM.plot <- function(survDf,  grouping.var = 'Group', surv.type = 'PFS', eve
   # Fit survival data using the Kaplan-Meier method
   survfit$call$formula <- fmla
   
-  palette <- c("blue",  "orange", "red",   "purple", "coral1", "brown")
+  if (is.null(palette)){
+  	palette <- c("blue",  "orange", "red",   "purple", "coral1", "brown")
   
-  if (length(unique(survDf[[grouping.var]])) == 2){
-    palette <- c("blue", "red")
-  } else if (length(unique(survDf[[grouping.var]])) == 3){
-    palette <- c("blue",  "orange", "red")
-  } else if (length(unique(survDf[[grouping.var]])) == 4){
+  	if (length(unique(survDf[[grouping.var]])) == 2){
+    	palette <- c("blue", "red")
+  	} else if (length(unique(survDf[[grouping.var]])) == 3){
+    	palette <- c("blue",  "orange", "red")
+  	} else if (length(unique(survDf[[grouping.var]])) == 4){
     palette <- c("blue", "mediumturquoise", "orange", "red")
-  }
-  
+  	}
+  } 
   p <- ggsurvplot(survfit, data = survDf, 
                   pval = FALSE,
-                  legend.title = '',
+                  #legend.title = '',
+                  legend="none",
+                  legend.labs=legendLabs,
                   risk.table=T, 
-                  fontsize=4,
+                  fontsize=5,
                   font.tickslab = 14,
-                  font.legend=13, 
+                  font.legend=9, 
                   palette=palette[1:length(unique(survDf[[grouping.var]]))],
                   xlab = time.unit, 
                   ylab = paste(surv.type, "Probability")
   )
   
   if (is.null(label.x)){
-    label.x <- max(survDf[[surv.type]], na.rm = TRUE) * 0.5
+    label.x <- max(survDf[[surv.type]], na.rm = TRUE) * 0.45
   }
   if (is.null(label.y)){
     label.y <- 0.96
@@ -66,11 +71,10 @@ make.KM.plot <- function(survDf,  grouping.var = 'Group', surv.type = 'PFS', eve
   p$plot <- p$plot + ggplot2::annotate(
     "text",
     x = label.x, y = label.y,
-    vjust  = 0.8, hjust = 0,
-    
-    label = paste0('HR = ', hr, ' (', hr.low95, '-', hr.high95, ')\nP = ', pvalue, ' (Logrank test)\nMedian ', surv.type, ':\n', medianLabel),
-    size = 4
-  ) + theme(legend.position = "none")     
+    vjust  = 1, hjust = 0,
+    #label = paste0('HR = ', hr, ' (', hr.low95, '-', hr.high95, ')\nP = ', pvalue, ' (Logrank test)\nMedian ', surv.type, ':\n', medianLabel),
+    label = paste0('HR = ', hr, ' (', hr.low95, '-', hr.high95, ')\nP = ', pvalue, '\nMedian ', surv.type, ':\n', medianLabel),
+    size = 4) + ggtitle(plot.title)
   p
 }
 
